49964e182bfd0eb6b543522aa41ba00037e94eb9
Lucas <lucasdvilar@gmail.com>
2021-06-29 18:24:49 -0300

    Continuando.

--- JavaScript/introducao-javascript/js/principal.js
+++ JavaScript/introducao-javascript/js/principal.js
@@ -1,32 +1,43 @@
 var titulo = document.querySelector('.titulo')
 titulo.textContent = 'Aparecida Nutricionista'
 
-var paciente = document.querySelector('#primeiro-paciente') //seleciona (no document) o id primeiro-paciente
+var pacientes = document.querySelectorAll('.paciente') //seleciona todos
 
-var tdPeso = paciente.querySelector('.info-peso') //seleciona (no primeiro-paciente) a class info-peso
-var peso = tdPeso.textContent //devolve apenas o conteúdo
+for (var i = 0; i < pacientes.length; i++) {
+  var paciente = pacientes[i]
 
-var tdAltura = paciente.querySelector('.info-altura')
-var altura = tdAltura.textContent
+  var tdPeso = paciente.querySelector('.info-peso') //seleciona (no primeiro-paciente) a class info-peso
+  var peso = tdPeso.textContent //devolve apenas o conteúdo
 
-var tdImc = paciente.querySelector('.info-imc')
+  var tdAltura = paciente.querySelector('.info-altura')
+  var altura = tdAltura.textContent
 
-var pesoValido = true
-var alturaValida = true
+  var tdImc = paciente.querySelector('.info-imc')
 
-if (peso <= 0 || peso >= 1000) {
-  console.log('Peso inválido!')
-  pesoValido = false
-  tdImc.textContent = 'Peso inválido!'
+  var pesoValido = true
+  var alturaValida = true
+
+  if (peso <= 0 || peso >= 1000) {
+    console.log('Peso inválido!')
+    pesoValido = false
+    tdImc.textContent = 'Peso inválido!'
+    paciente.classList.add('paciente-invalido')
+  }
+
+  if (altura <= 0 || altura >= 3.0) {
+    console.log('Altura inválida!')
+    alturaValida = false
+    tdImc.textContent = 'Altura inválida!'
+    paciente.classList.add('paciente-invalido')
+  }
+
+  if (pesoValido && alturaValida) {
+    var imc = peso / (altura * altura)
+    tdImc.textContent = imc.toFixed(2)
+  }
 }
 
-if (altura <= 0 || altura >= 3.0) {
-  console.log('Altura inválida!')
-  alturaValida = false
-  tdImc.textContent = 'Altura inválida!'
-}
-
-if (pesoValido && alturaValida) {
-  var imc = peso / (altura * altura)
-  tdImc.textContent = imc
-}
+var botaoAdicionar = document.querySelector('#adicionar-paciente')
+botaoAdicionar.addEventListener('click', function () {
+  console.log('Oi, cliquei!')
+})
