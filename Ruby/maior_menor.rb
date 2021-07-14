4d69adb4fd4a1d58cf01371cf94a2150d1231dc1
Lucas <lucasdvilar@gmail.com>
2021-07-14 18:18:16 -0300

    Continuando.

--- Ruby/maior_menor.rb
+++ Ruby/maior_menor.rb
@@ -1,2 +1,14 @@
+puts "Hello, world!"
 puts "Bem vindo ao jogo de adivinhação"
 
+puts "Qual é seu nome?"
+name = gets
+puts "O jogo vai começar, " + name
+
+puts "Escolha um num entre 1 e 200"
+secretNumber = 175
+
+puts "Tentativa 1"
+chute = gets
+puts "Será que acertou? Você chutou " + chute
+puts chute.to_i == secretNumber
